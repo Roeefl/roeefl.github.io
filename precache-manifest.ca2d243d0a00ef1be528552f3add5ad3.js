269f0852f2cd087be4b130b0f30171e04977f244
Roee FL <Roeefl@gmail.com>
2019-04-13 21:31:22 +0300

    create-react-app build

---
+++ precache-manifest.ca2d243d0a00ef1be528552f3add5ad3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1a44d7e5e782b25ce991",
+    "url": "/static/js/main.d16230f7.chunk.js"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "1a44d7e5e782b25ce991",
+    "url": "/static/css/main.566c093d.chunk.css"
+  },
+  {
+    "revision": "b0f456b4142b16409bcd9c1e8b1e82a3",
+    "url": "/index.html"
+  }
+];
