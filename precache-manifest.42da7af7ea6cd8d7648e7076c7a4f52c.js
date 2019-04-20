5f1ca0b1d1857e2d8fb90325cc150bc039e0f581
Roee FL <Roeefl@gmail.com>
2019-04-21 00:16:24 +0300

    Firebase, Bootstrap

---
+++ precache-manifest.42da7af7ea6cd8d7648e7076c7a4f52c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f70b17ba03ab7a856085085d59fa6e3",
+    "url": "/static/media/logo.7f70b17b.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7bebae888c18aeaea3aa",
+    "url": "/static/js/main.5d1e4add.chunk.js"
+  },
+  {
+    "revision": "0b0a7c3811b342f88572",
+    "url": "/static/js/2.fefc0364.chunk.js"
+  },
+  {
+    "revision": "7bebae888c18aeaea3aa",
+    "url": "/static/css/main.e36bcd0c.chunk.css"
+  },
+  {
+    "revision": "6d347a5bca2a101e0d2cc62b933371cd",
+    "url": "/index.html"
+  }
+];
